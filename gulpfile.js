5bd0c43c35714d747b54d1353971fcb04480e3cc
Yash Sankhala <yash.sankhala@impetus.co.in>
2018-06-18 17:52:19 +0530

    optimizing css and javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('hello', function() {
     console.log('hello world')
@@ -28,4 +32,12 @@ gulp.task('browserSync', function() {
             baseDir: 'app'
         }
     })
+})
+
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
 })
